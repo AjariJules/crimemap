1fed7d2adab79a68e98c9503a4ee1a8e3c7d2f31
Ajari Jules <ajarinjules@gmail.com>
2017-09-08 22:41:04 -0400

    Added multiple markers capabilities via js. need to add validation

--- crimemap.py
+++ crimemap.py
@@ -1,18 +1,16 @@
 from dbhelper import DBHelper
 from flask import Flask, render_template, request
-
+import json
 app = Flask(__name__)
 DB = DBHelper()
 api = DB.getApi()
 
+
 @app.route("/")
 def home():
-    try:
-        data = DB.get_all_inputs()
-    except Exception as e:
-        print (e)
-        data = None
-    return  render_template("home.html", data= data,api = api)
+    crimes = DB.get_all_crimes()
+    crimes = json.dumps(crimes)
+    return render_template("home.html", api=api, crimes = crimes)
 
 
 @app.route("/add", methods=["POST"])
@@ -24,12 +22,24 @@ def add():
         print (e)
     return home()
 
+
 @app.route("/clear")
 def clear():
     try:
         DB.clear_all()
     except Exception as e:
         print (e)
+    return home()
+
+
+@app.route("/submitcrime", methods=['POST'])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 if __name__ == '__main__':
--- dbhelper.py
+++ dbhelper.py
@@ -1,10 +1,10 @@
 import pymysql
 import dbconfig
+import  datetime
 
 
 class DBHelper:
-
-    def connect(self, database ="crimemap"):
+    def connect(self, database="crimemap"):
         return pymysql.connect(host='localhost',
                                user=dbconfig.db_user,
                                passwd=dbconfig.db_password,
@@ -16,7 +16,7 @@ class DBHelper:
             query = "SELECT api FROM api;"
             with connection.cursor() as cursor:
                 cursor.execute(query)
-            return  cursor.fetchone()[0]
+            return cursor.fetchone()[0]
         finally:
             connection.close()
 
@@ -30,14 +30,51 @@ class DBHelper:
         finally:
             connection.close()
 
-
     def add_input(self, data):
         connection = self.connect()
         try:
             query = "INSERT INTO crimes (description) VALUES (%s);"
             with connection.cursor() as cursor:
-                cursor.execute(query,data)
+                cursor.execute(query, data)
                 connection.commit()
+        finally:
+            connection.close()
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        connection = self.connect()
+
+        try:
+            query = "INSERT INTO crimes (category, date, latitude, longitude, description) " \
+                    "VALUES( % s, % s, % s, % s, % s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (category, date, latitude, longitude, description))
+            connection.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            connection.close()
+
+    def get_all_crimes(self):
+        connection = self.connect()
+
+        try:
+            query = "SELECT latitude, longitude, date, category, description FROM crimes;"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+
+            named_crimes = []
+            for crime in result:
+                named_crime = {
+                    'latitude': crime[0],
+                    'longitude': crime[1],
+                    'date': datetime.datetime.strftime(crime[2], '%Y- %m-%d'),
+                    'category': crime[3],
+                    'description': crime[4]
+                }
+                named_crimes.append(named_crime)
+            print (named_crimes)
+            return named_crimes
         finally:
             connection.close()
 
@@ -50,4 +87,3 @@ class DBHelper:
                 connection.commit()
         finally:
             connection.close()
-
